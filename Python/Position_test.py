2a931426963fd4fe119e530e0f941746055d31e7
Krzysztof Żybura <57592725+Krzzyb@users.noreply.github.com>
2021-05-09 13:26:54 +0200

    simple test
    * test for size4
    
    * result check
    
    Co-authored-by: Marcin Zybura <zybura.marcin@gmail.com>

--- Python/Position_test.py
+++ Python/Position_test.py
@@ -20,3 +20,19 @@ print(f"D3.isOnlyTwo([B2])={result}")
 visitedMoves = [Position("B2"),Position("C1")]
 result = p1.isOnlyTwo(3, visitedMoves)
 print(f"D3.isOnlyTwo([B2,C1])={result}")
+
+# allowed moves test
+print("Allowed moves test:")
+startPoint = Position("B1")
+boardSize = 4
+b1SampleResult = [
+  Position("B1"),
+  Position("A3"),
+  Position("C3"),
+  Position("D2")
+]
+
+moves = startPoint.getAllFairAllowedMoves (boardSize, b1SampleResult)
+print(f"{[str(move) for move in moves]}")
+print("Expected moves:[D2, C3, A3]")
+
---
+++ Python/findBestMove_test_size4.py
@@ -0,0 +1,33 @@
+from findBestMove import findBestMove
+from Position import Position
+from result_check import resultCheck
+
+# Arrange
+startPoint = Position("B1")
+boardSize = 4
+b1SampleResult = [
+  Position("B1"),
+  Position("D2"),
+  Position("B3"),
+  Position("D4"),
+  Position("C2"),
+  Position("B4"),
+  Position("D3"),
+  Position("B2"),
+  Position("A4"),
+  Position("C3")
+]
+
+# Run
+moves = findBestMove(startPoint, startPoint, boardSize, [])
+
+# Assert
+print(f"Expected Moves: {[str(move) for move in b1SampleResult]}")
+print(f"Returned Moves: {[str(move) for move in moves]}")
+print("Result check:")
+print(resultCheck(moves, boardSize))
+
+startPoint = Position("D3")
+moves = findBestMove(startPoint, startPoint, boardSize, [])
+print(f"Returned Moves: {[str(move) for move in moves]}")
+print(resultCheck(moves, boardSize))
